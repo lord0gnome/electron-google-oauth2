e618c0c80332cdc3ef48f2cf384c68fa6ead1bea
Guillaume Richard <lord0gnome@gmail.com>
2021-09-30 17:20:42 +0200

    removed in browser redirect

--- src/LoopbackRedirectServer.ts
+++ src/LoopbackRedirectServer.ts
@@ -25,9 +25,6 @@ export default class LoopbackRedirectServer {
     this._maybeRedirection = new Promise((resolve, reject) => {
       this._server = http.createServer((req, res) => {
         if (req.url && url.parse(req.url).pathname === callbackPath) {
-          res.writeHead(302, {
-            Location: successRedirectURL,
-          });
           res.end();
 
           resolve(url.resolve(`http://127.0.0.1:${port}`, req.url));
